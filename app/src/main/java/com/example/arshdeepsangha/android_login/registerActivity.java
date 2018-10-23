f8bd75e605d1589aab496533c7f80cb6d196591d
arsh98000 <arsh98000@gmail.com>
2018-10-23 17:03:33 -0500

    Added validation and functionality to Register Activity.

--- app/src/main/java/com/example/arshdeepsangha/android_login/registerActivity.java
+++ app/src/main/java/com/example/arshdeepsangha/android_login/registerActivity.java
@@ -1,13 +1,63 @@
 package com.example.arshdeepsangha.android_login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class registerActivity extends AppCompatActivity {
+
+    private EditText etUsername,etPasswordRegister,etRePassword;
+    private String username;
+    private String confirmPassword;
+    private String password;
+    private Button btnRegisterAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPasswordRegister = findViewById(R.id.etPasswordRegister);
+        etRePassword = findViewById(R.id.etRePassword);
+        btnRegisterAccount = findViewById(R.id.btnRegisterAccount);
+
+        getValues();
+
+        btnRegisterAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getValues();
+                validate();
+            }
+        });
+
+    }
+
+    private void getValues()
+    {
+        username = etUsername.getText().toString();
+        password = etPasswordRegister.getText().toString();
+        confirmPassword = etRePassword.getText().toString();
+    }
+
+    private void validate()
+    {
+        if(username.equals("") || password.equals("") || confirmPassword.equals(""))
+        {
+            Toast.makeText(this,"Please fill in the requirements", Toast.LENGTH_LONG).show();
+        }
+        else if(password != confirmPassword)
+        {
+            Toast.makeText(this,"Passwords do not match!", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+
+        }
     }
 }
