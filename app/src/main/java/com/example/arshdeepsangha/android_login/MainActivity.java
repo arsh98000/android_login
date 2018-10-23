8e7cf0de77e67eb98c044242fd20ae7623475813
arsh98000 <arsh98000@gmail.com>
2018-10-23 14:26:00 -0500

    Updated the edit text validations.

--- app/src/main/java/com/example/arshdeepsangha/android_login/MainActivity.java
+++ app/src/main/java/com/example/arshdeepsangha/android_login/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.arshdeepsangha.android_login;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                validate();
+            }
+        });
+    }
+
+    private void validate()
+    {
+        if (etName.getText().toString().equals("") || etPassword.getText().toString().equals(""))
+        {
+            Toast.makeText(this,"Please fill in the requirements", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Intent intent = new Intent(this,successfulActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/arshdeepsangha/android_login/successfulActivity.java
@@ -0,0 +1,14 @@
+package com.example.arshdeepsangha.android_login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class successfulActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful);
+    }
+}
