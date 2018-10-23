6a2550554ae1a45731be3e00c0554c69c5545a52
arsh98000 <arsh98000@gmail.com>
2018-10-23 13:52:37 -0500

    Made the layout of mainActivity and assigned the id with widgets.

--- app/src/main/java/com/example/arshdeepsangha/android_login/MainActivity.java
+++ app/src/main/java/com/example/arshdeepsangha/android_login/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.arshdeepsangha.android_login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etName;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnRegister;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
     }
 }
